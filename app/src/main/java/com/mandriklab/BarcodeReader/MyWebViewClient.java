c7ddc8610152328d8e71054a8e737067911c4199
JeZ <JeZ@DESKTOP-MEITL6E>
2019-01-30 23:04:58 +0300

    first publication

---
+++ app/src/main/java/com/mandriklab/BarcodeReader/MyWebViewClient.java
@@ -0,0 +1,26 @@
+package com.mandriklab.BarcodeReader;
+
+import android.webkit.WebViewClient;
+
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+
+class MyWebViewClient extends WebViewClient {
+
+    @TargetApi(Build.VERSION_CODES.N)
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+        view.loadUrl(request.getUrl().toString());
+        return true;
+    }
+
+    // Для старых устройств
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        view.loadUrl(url);
+        return true;
+    }
+}
